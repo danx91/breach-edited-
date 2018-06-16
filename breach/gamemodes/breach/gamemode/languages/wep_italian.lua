10e3a0deb1187a369cc9dd930493dfed334d5156
FrostCop <39341256+FrostCop@users.noreply.github.com>
2018-06-16 14:41:41 +0300

    Created wep_italian.lua
    Created wep_italian.lua by FrostCop

---
+++ breach/gamemodes/breach/gamemode/languages/wep_italian.lua
@@ -0,0 +1,364 @@
+lang = {}
+
+lang.SCP_0082 = {
+	author = "Kanade, modificato da danx91",
+	contact = "Guarda la sua modalità nel workshop e cerca i creatori",
+	purpose = "Infettare",
+	instructions = "LMB per attaccare",
+}
+
+lang.SCP_023 = {
+	author = "danx91",
+	contact = "Guarda la sua modalità nel workshop e cerca i creatori",
+	purpose = "Uccidere",
+	instructions = "LMB per attaccare",
+	HUD = {
+		lowhealth = "La tua vita è troppo bassa",
+	},
+}
+
+lang.SCP_049 = {
+	author = "Kanade",
+	contact = "Guarda la sua modalità nel workshop e cerca i creatori",
+	purpose = "'Curare'",
+	instructions = "LMB per 'curare' qualcuno",
+	HUD = {
+		attackReady = "Pronto per attaccare",
+		attackCD = "Prossimo attacco in",
+	},
+}
+
+lang.SCP_066 = {
+	author = "danx91",
+	contact = "Guarda la sua modalità nel workshop e cerca i creatori",
+	purpose = "Uccidere",
+	instructions = "LMB per emettere suoni",
+	HUD = {
+		attackReady = "Pronto per attaccare",
+		attackCD = "Prossimo attacco in",
+	},
+}
+
+lang.SCP_082 = {
+	author = "danx91, armi da M9K Specialities",
+	contact = "Guarda la sua modalità nel workshop e cerca i creatori",
+	purpose = "Uccidere",
+	instructions = "LMB per attaccare",
+}
+
+lang.SCP_076 = {
+	author = "danx91",
+	contact = "Guarda la sua modalità nel workshop e cerca i creatori",
+	purpose = "Uccidere",
+	instructions = "LMB per attaccare",
+}
+
+lang.SCP_096 = {
+	author = "Vinrax, edited by danx91",
+	contact = "Guarda la sua modalità nel workshop e cerca i creatori",
+	purpose = "Uccidere",
+	instructions = "LMB per attaccare",
+}
+
+lang.SCP_106 = {
+	author = "Kanade",
+	contact = "Guarda la sua modalità nel workshop e cerca i creatori",
+	purpose = "Uccidere",
+	instructions = "LMB per teletrasportare qualcuno alla dimensione portatile\nRMB per piazzare il teletrasporto\nR per teletrasportarti alla posizione marcata",
+	HUD = {
+		attackReady = "Pronto per attaccare",
+		attackCD = "Prossimo attacco in",
+		teleportReady = "Teletrasporto pronto",
+		teleportCD = "Prossimo teltrasporto in"
+	},
+}
+
+lang.SCP_173 = {
+	author = "Kanade",
+	contact = "Guarda la sua modalità nel workshop e cerca i creatori",
+	purpose = "Uccidere",
+	instructions = "LMB per uccidere qualcuno, RMB per accecare i tuoi obbiettivi",
+	HUD = {
+		nlook = "Nessuno stà guardando",
+		specCD = "Speciale pronta da usare in",
+		specReady = "Speciale pronta da usare",
+		slook = "Qualcuno stà guardando",
+	}
+}
+
+lang.SCP_420j = {
+	author = "danx91",
+	contact = "Guarda la sua modalità nel workshop e cerca i creatori",
+	purpose = "La migliore droga nel mondo",
+	instructions = "MAN DATS SUM GOOD ASS SHIT",
+}
+
+lang.SCP_457 = {
+	author = "Kanade",
+	contact = "Guarda la sua modalità nel workshop e cerca i creatori",
+	purpose = "Brucia",
+	instructions = "",
+}
+
+lang.SCP_500 = {
+	author = "danx91",
+	contact = "Guarda la sua modalità nel workshop e cerca i creatori",
+	purpose = "Curarti",
+	instructions = "LMB per usarla, o sarà automaticamente attivata quando riceverai colpi mortali",
+}
+
+lang.SCP_682 = {
+	author = "danx91",
+	contact = "Guarda la sua modalità nel workshop e cerca i creatori",
+	purpose = "Uccidere",
+	instructions = "LMB per attaccare, RMB per usare l'abilità",
+	HUD = {
+		attackReady = "Abilità pronta",
+		attackCD = "Abilità pronta in",
+	},
+}
+
+lang.SCP_689 = {
+	author = "danx91",
+	contact = "Guarda la sua modalità nel workshop e cerca i creatori",
+	purpose = "Uccidere",
+	instructions = "LMB per teletrasportarti al tuo obbiettivo",
+	HUD = {
+		attackReady = "Pronto per attaccare",
+		attackCD = "Prossimo attacco in",
+		targets = "Obbiettivi"
+	},
+}
+
+lang.SCP_714 = {
+	author = "danx91",
+	contact = "Guarda la sua modalità nel workshop e cerca i creatori",
+	purpose = "Ti protegge",
+	instructions = "Mantieni per usare",
+	HUD = {
+		durability = "Durabilità:",
+		protect = "Sei protetto",
+		protend = "La tua protezione finirà presto",
+	},
+}
+
+lang.SCP_8602 = {
+	author = "danx91",
+	contact = "Guarda la sua modalità nel workshop e cerca i creatori",
+	purpose = "Uccidere",
+	instructions = "LMB per attaccare",
+}
+
+lang.SCP_939 = {
+	author = "danx91",
+	contact = "Guarda la sua modalità nel workshop e cerca i creatori",
+	purpose = "Uccidere",
+	instructions = "LMB per attaccare, RMB per cambiare chat vocale",
+	HUD = {
+		attackReady = "Pronto per attaccare",
+		attackCD = "Prossimo attacco in",
+		channel = "Chat vocale corrente:",
+	},
+}
+
+lang.SCP_957 = {
+	author = "danx91",
+	contact = "Guarda la sua modalità nel workshop e cerca i creatori",
+	purpose = "Uccidere",
+	instructions = "LMB per fare danno ad area",
+	HUD = {
+		rattack = "Attacco Pronto",
+		nattack = "Prossimo attacco in",
+		nsummon = "Transformazione in SCP-957-1 in",
+		asummon = "L' SCP-957-1 è vivo",
+	},
+}
+
+lang.SCP_966 = {
+	author = "Kanade, edited by danx91",
+	contact = "Guarda la sua modalità nel workshop e cerca i creatori",
+	purpose = "Uccidere",
+	instructions = "Fai danno quando sei vicino agli umani",
+}
+
+lang.SCP_999 = {
+	author = "danx91",
+	contact = "Guarda la sua modalità nel workshop e cerca i creatori",
+	purpose = "Curare tutti",
+	instructions = "LMB per curare l'obbiettivo, RMB per curare tutti quelli vicini",
+	HUD = {
+		healPronto = "La cura è pronta",
+		healCD = "La cura è in cooldown. Aspetta",
+		ghealPronto = "La cura di gruppo è prnta",
+		ghealCD = "La cura di gruppo è in cooldown. Aspetta",
+	},
+}
+
+lang.SCP_1048A = {
+	author = "danx91",
+	contact = "Guarda la sua modalità nel workshop e cerca i creatori",
+	purpose = "Uccidere",
+	instructions = "LMB per emettere un forte urlo",
+	HUD = {
+		attackReady = "Pronto per attaccare",
+		attackCD = "Prossimo attacco in",
+	},
+}
+
+lang.SCP_1048B = {
+	author = "danx91",
+	contact = "Guarda la sua modalità nel workshop e cerca i creatori",
+	purpose = "Uccidere",
+	instructions = "LMB per attaccare",
+}
+
+lang.SCP_1471 = {
+	author = "danx91",
+	contact = "Guarda la sua modalità nel workshop e cerca i creatori",
+	purpose = "Uccidere",
+	instructions = "LMB per attaccare, RMB per teletrasportarti",
+	HUD = {
+		attackReady = "Pronto per attaccare",
+		attackCD = "Prossimo attacco in",
+	},
+}
+
+lang.SCP_SantaJ = {
+	author = "danx91",
+	contact = "Guarda la sua modalità nel workshop e cerca i creatori",
+	purpose = "Dare regali",
+	instructions = "LMB per lanciare regali",
+	HUD = {
+		attackReady = "Pronto per attaccare",
+		attackCD = "Prossimo attacco in",
+		gtype = "Tipo del regalo:",
+		explosive = "Esplosivo",
+		healing = "Curativo",
+	},
+}
+
+lang.HOLSTER = {
+	author = "Kanade",
+	contact = "Guarda la sua modalità nel workshop e cerca i creatori",
+	purpose = "Disarmato",
+	instructions = "",
+}
+
+lang.ID = {
+	author = "Kerry, edited by danx91",
+	contact = "Aggiungi su Steam",
+	purpose = "Mostra il tuo ID Tag agli altri",
+	instructions = "",
+	name = "Nome:",
+	city = "Città:",
+}
+
+lang.CV = {
+	author = "Kanade",
+	contact = "Guarda la sua modalità nel workshop e cerca i creatori",
+	purpose = "Guarda alle telecamere",
+	instructions = "Se lo mantieni clicca RMB per cambiare visuale",
+	changed = "Cambiato a"
+}
+
+lang.EYEDROPS = {
+	author = "Kanade",
+	contact = "Guarda la sua modalità nel workshop e cerca i creatori",
+	purpose = "Smetti di sbattere le palpebre per 10 secondi",
+	instructions = "Click sinistro per usare",
+}
+
+lang.MEDKIT = {
+	author = "Kanade",
+	contact = "Guarda la sua modalità nel workshop e cerca i creatori",
+	purpose = "Curati o cura gli altri",
+	instructions = "Click sisinistro per curarti, destro per curare gli altri",
+}
+
+lang.NVG = {
+	author = "Kanade",
+	contact = "Guarda la sua modalità nel workshop e cerca i creatori",
+	purpose = "Ti dà visione notturna",
+	instructions = "Mantienilo",
+}
+
+lang.RADIO = {
+	author = "Kanade",
+	contact = "Guarda la sua modalità nel workshop e cerca i creatori",
+	purpose = "Comunicare",
+	instructions = "Se lo tieni, i tuoi messaggi vocali e la chat sarà globalo con gli altri che hanno la radio",
+	channel = "Canale della radio",
+	disabled = "Radio disabilitata",
+}
+
+lang.SNAV = {
+	author = "Kanade",
+	contact = "Guarda la sua modalità nel workshop e cerca i creatori",
+	purpose = "Trovare una via da seguire",
+	instructions = "Se lo tieni clicca RMB per cambiare visuale",
+}
+
+lang.SNAV_ULT = {
+	author = "Kanade",
+	contact = "Guarda la sua modalità nel workshop e cerca i creatori",
+	purpose = "Trovare una via da seguire e scannerizzare nemici",
+	instructions = "Se lo tieni clicca RMB per cambiare visuale",
+	mtfdetect = "Guardia MTF rilevata",
+	cidetect = "Membro Chaos Insurgency rilevato",
+	resdetect = "Ricercatore rilevato",
+	ddetect = "Classe D rilevato",
+	unkdetect = "Forma umanoide rilevata",
+	detect = "rilevato",
+	items = "Oggetti rivelati:"
+}
+
+lang.KEYCARD = {
+	author = "danx91",
+	contact = "Guarda la sua modalità nel workshop e cerca i creatori",
+	purpose = "Apre porte che richiedono keycard (Usa il tasto per ricaricare per vedere dove disponde l'accesso)",
+	instructions = "Accesso:",
+	ACC = {
+		"SAFE",
+		"EUCLID",
+		"KETER",
+		"Checkpoints",
+		"OMEGA Warhead",
+		"Portal Obs.",
+		"Gate A",
+		"Gate B",
+		"Armeria",
+		"Femur Breaker",
+		"EC",
+	},
+	STATUS = {
+		"ACCESSO",
+		"NO ACCESSO",
+	},
+	NAMES = {
+		"Keycard Livello 1",
+		"Keycard Livello 2",
+		"Keycard Livello 3",
+		"Keycard Ricercatore",
+		"Keycard Guardia MTF",
+		"Keycard Comandante MTF",
+		"Keycard Livello OMNI",
+		"Checkpoint Security Keycard",
+		"Keycard CI Hackerata",
+	},
+}
+
+lang.ZEUS = {
+	author = "BFG9000, modificato da danx91",
+	contact = "Pagina Workshop",
+	purpose = "Emette una scarica letale di elettricità a raggio ravvicinato",
+	instructions = "Devi essere vicino per uccidere.",
+}
+
+lang.ZEUS_EXP = {
+	author = "PROTOTIPO",
+	contact = "PROTOTIPO",
+	purpose = "PROTOTIPO",
+	instructions = "PROTOTIPO",
+}
+
+WEPLANG.italian = lang
